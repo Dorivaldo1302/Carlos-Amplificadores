e37319483eebf19d413724e0e97a461580ec4e66
Dorivaldo1302 <delciof1609@gmail.com>
2024-11-22 13:14:47 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function ativar(){
+  document.querySelector('.menu').classList.toggle('ativo')
+  document.querySelector('nav').classList.toggle ('link')
+}
+function remover(){
+  document.querySelector('nav').classList.remove('link')
+}
